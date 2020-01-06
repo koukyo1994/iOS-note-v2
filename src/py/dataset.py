03084c9826d9316ab96f4f4e9cf203e27b0713fd
Hidehisa Arai <ge97cup@mytum.de>
2020-01-06 18:11:55 +0100

    add image generation code

--- src/py/dataset.py
+++ src/py/dataset.py
@@ -1,8 +1,12 @@
+import hashlib
 import subprocess
 
 import imgaug.augmenters as iaa
 import numpy as np
+import pandas as pd
 
+from argparse import ArgumentParser
+from pathlib import Path
 from PIL import Image, ImageDraw, ImageFont
 from typing import Tuple
 
@@ -46,7 +50,7 @@ def choose_word() -> str:
     return np.random.choice(WORDS)
 
 
-def choose_font(font_size_range=(15, 30)) -> ImageFont.FreeTypeFont:
+def choose_font(font_size_range=(40, 46)) -> ImageFont.FreeTypeFont:
     fontname = _choose_font_name()
     fontsize = np.random.randint(*font_size_range)
     return ImageFont.truetype(fontname, size=fontsize)
@@ -58,23 +62,62 @@ def generate_image(height: int,
     text = choose_word()
 
     textsize_x, textsize_y = font.getsize(text)
-    if textsize_y > height:
+    if textsize_y > height * 2:
         raise ValueError(
             f"Given height is not enough for the fontsize {font.size}")
     width = textsize_x + 15
 
     x = np.random.randint(0, width - textsize_x)
-    y = np.random.randint(0, height - textsize_y)
+    y = np.random.randint(0, height * 2 - textsize_y)
 
-    image = Image.new("RGB", (width, height), color=(255, 255, 255))
+    image = Image.new("RGB", (width, height * 2), color=(255, 255, 255))
     draw = ImageDraw.Draw(image)
     draw.text((x, y), text, fill=(0, 0, 0), font=font)
     if noise:
         img = AUGMENTOR.augment_image(np.asarray(image))
         image = Image.fromarray(img)
+        image = image.resize((width // 2, height))
     return image, text
 
 
 if __name__ == "__main__":
-    image, text = generate_image(height=32, noise=True)
-    image.save("images/sample.png")
+    parser = ArgumentParser()
+    parser.add_argument("--n_samples", type=int, required=True)
+
+    args = parser.parse_args()
+
+    output_dir = Path("data")
+    images_dir = output_dir / "images"
+    if args.n_samples < 1:
+        raise ValueError(
+            f"Number of samples must be over 1. Got {args.n_samples}")
+    elif args.n_samples == 1:
+        image, text = generate_image(height=32, noise=True)
+        image.save(images_dir / "sample.png")
+    else:
+        texts = []
+        names = []
+        heights = []
+        widths = []
+        for i in range(args.n_samples):
+            try:
+                name = hashlib.md5(str(i).encode("utf-8")).hexdigest()
+                image, text = generate_image(height=32, noise=True)
+                width, height = image.size
+
+                names.append(name)
+                texts.append(text)
+                widths.append(width)
+                heights.append(height)
+
+                image.save(images_dir / f"{name}.png")
+            except ValueError:
+                pass
+
+        labels = pd.DataFrame({
+            "image_id": names,
+            "text": texts,
+            "width": widths,
+            "height": heights
+        })
+        labels.to_csv(output_dir / "labels.csv", index=False)
