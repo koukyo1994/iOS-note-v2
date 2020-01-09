5f72185d38b47daa030b0022867a9b5adbf31002
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-01-09 11:20:10 +0100

    add prediction.py

---
+++ src/py/prediction.py
@@ -0,0 +1,30 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+from create_dataset import generate_image
+from dataset import CHARS
+from model import get_model
+
+
+def predict_text(image: np.ndarray, model: tf.keras.Model) -> str:
+    height = image.shape[0]
+    image = cv2.resize(image, (int(image.shape[1] / height * 32), 32))
+    input = (image.astype(np.float32) / 127.5 - 1.0)[np.newaxis, :, :, :]
+    pred = model.predict(input)
+    decoded, _ = tf.keras.backend.ctc_decode(pred, [input.shape[2] // 4])
+    return "".join((CHARS[idx - 1] for idx in decoded[0][0].numpy()))
+
+
+if __name__ == "__main__":
+    model = get_model(input_shape=(32, None, 3), n_vocab=len(CHARS) + 1)
+    model.load_weights("bin/model_weights.h5")
+
+    image, text = generate_image(height=32, noise=False)
+    img = np.asarray(image)
+
+    pred = predict_text(img, model)
+    plt.imshow(img)
+    plt.title(f"Answer: {text}, Pred: {pred}")
+    plt.savefig(f"data/prediction/pred_{text}.png")
