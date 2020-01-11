55ed045031f1bf0cd6dfd86732e0d6a16be7dfc1
koukyo1994 <arabiannight1994@yahoo.co.jp>
2020-01-11 14:31:05 +0100

    add ViewController

--- src/NoteApp/NoteApp/ViewController.swift
+++ src/NoteApp/NoteApp/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let navBarCoefficient: CGFloat = 0.02
+    
+    let canvasView = CanvasView()
+    let ruledLineView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // background
+        ruledLineView.backgroundColor = .white
+        ruledLineView.frame = getCanvasFrame()
+        ruledLineView.isUserInteractionEnabled = false
+        ruledLineView.clipsToBounds = true
+        view.addSubview(ruledLineView)
+        
+        // canvas
+        canvasView.backgroundColor = .clear
+        canvasView.frame = getCanvasFrame()
+        canvasView.isUserInteractionEnabled = true
+        canvasView.clipsToBounds = true
+        view.addSubview(canvasView)
+        setNavigationBar()
+    }
+    
+    func setNavigationBar() {
+        let screenSize: CGRect = UIScreen.main.bounds
+        let navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height * navBarCoefficient))
+        let navItem = UINavigationItem(title: "Navigation Area")
+        let barButton = UIBarButtonItem(title: "clear", style: .plain, target: self, action: #selector(self.clear))
+        navItem.rightBarButtonItem = barButton
+        navBar.setItems([navItem], animated: false)
+        view.addSubview(navBar)
     }
 
-
+    func getCanvasFrame() -> CGRect {
+        let screenSize = UIScreen.main.bounds
+        return CGRect(x: 0, y: screenSize.height * navBarCoefficient, width: screenSize.width, height: screenSize.height * (1 - navBarCoefficient))
+    }
+    
+    @objc private func clear(_ sender: UIBarButtonItem) {
+        canvasView.clear()
+    }
 }
 
